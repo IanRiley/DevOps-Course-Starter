e8f49642b828934e7f51fefa50d2a59df3f4779f
IanRiley <69636250+IanRiley@users.noreply.github.com>
2020-10-16 16:02:10 +0100

    Changes following feedback and additional functions
    Changes following feedback and additional functions

--- app.py
+++ app.py
@@ -1,27 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
-
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+#changed to the template for local host v2.0
+#unable to complete the sort by status function 
+#new branch created prior to submission
+
+#update from work 2.0
 @app.route('/')
 def index():
-    todo=session.get_items()
-    return render_template('index.html',all_todo=todo)
-
-@app.route('/', methods=["POST"])
+    items = session.get_items()
+    return render_template('index.html', todos = items)
+    
+@app.route('/add', methods=["POST"])
 def add_todo():
-    td_new_title=request.form.get('td_Title')
-    if (len(td_new_title) > 1 ):
-        session.add_item(td_new_title)
-    return index()
-#    todo=session.get_items()
-#    return render_template('index.html',all_todo=todo)
+    item = request.form.get('todo_task')
+    session.add_item(item)
+    return redirect(url_for('index'))
 
-@app.route('/del', methods=['POST']) 
-def del_book():     
-# Code to create a new book entry in the database. 
-    return render_template('message.html')
+#delete function 
+@app.route('/delete', methods=["POST"])
+def delete_todo():
+    item = request.form.get('todo_id')
+    print(item)
+    session.delete_item(item)
+    return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run()
+#update function 
+@app.route('/update', methods=["POST"])
+def update_todo():
+    item = request.form.get('todo_id')
+    new_todo_value = request.form.get("title")
+    new_status_value = request.form.get("status")
+    session.update_item(item, new_todo_value, new_status_value)
+    return redirect(url_for('index'))
